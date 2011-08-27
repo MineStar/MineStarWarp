bcdab1b002c9bbb27111afb9526a4de890d9ce80
Meldanor <Bhaals@gmx.de>
2011-08-27 13:24:52 +0200

    GPL is now licensed under GPL Fixed compile errors ( Refractoring in Eclipse isn't as powerfull as in NetBeans)
    Signed-off-by: Meldanor <Bhaals@gmx.de>

--- src/com/minestar/MineStarWarp/dataManager/HomeManager.java
+++ src/com/minestar/MineStarWarp/dataManager/HomeManager.java
@@ -1,4 +1,22 @@
-package com.minestar.MineStarWarp.data;
+/*
+ * Copyright (C) 2011 MineStar.de 
+ * 
+ * This file is part of MineStarWarp.
+ * 
+ * MineStarWarp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * MineStarWarp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MineStarWarp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.minestar.MineStarWarp.dataManager;
 
 import java.util.TreeMap;
 
